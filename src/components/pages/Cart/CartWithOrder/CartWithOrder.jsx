938dbbc105b6d1a24dc139963c395910ff0e5bb2
Roman Riezanov <romark6@gmail.com>
2022-09-12 16:13:09 +0300

    added adaptive to cart page and changed some styles

--- src/components/pages/Cart/CartWithOrder/CartWithOrder.jsx
+++ src/components/pages/Cart/CartWithOrder/CartWithOrder.jsx
@@ -1,131 +1,72 @@
 import React from "react";
+import CartSvgGenerator from "../../../../icons/CartSvgGenerator";
 import classes from "./CartWithOrder.module.scss";
 
 const CartWithOrder = () => {
   return (
-    <div class="container container--cart">
-      <div class="cart">
-        <div class={classes.cart__top}>
-          <h2 class={classes.content__title}>
-            <img src="./assets/img/icons/cart.svg" alt="cart" /> Корзина
-          </h2>
-          <div class={classes.cart__clear}>
-            <img src="./assets/img/icons/trash.svg" alt="trash" />
-            <span>Очистить корзину</span>
-          </div>
+    <div class="cart">
+      <div class={classes.cart__top}>
+        <h2 class={classes.content__title}>
+          <img src="./assets/img/icons/cart.png" alt="cart" /> Корзина
+        </h2>
+        <div class={classes.cart__clear}>
+          <CartSvgGenerator id="trash" />
+          <span>Очистить корзину</span>
         </div>
-        <div class={classes.content__item}>
-          <div className={classes.cart__item}>
-            <div className={classes.cart__item_img}>
-              <img src="./assets/img/sets/set_banzai.png" alt="cartItem" />
-            </div>
-            <div class="cart__item-info">
-              <h3>Сырный цыпленок</h3>
-              <p>тонкое тесто, 26 см.</p>
-            </div>
-            <div class={classes.cart__item_count}>
-              <div
-                class={`button button--outline button--circle ${classes.cart__item_count_minus}`}
-              >
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
-              </div>
-              <b>2</b>
-              <div
-                class={`button button--outline button--circle ${classes.cart__item_count_plus}`}
-              >
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div class={classes.cart__item_price}>
-              <b>770 ₽</b>
-            </div>
-            <div class={classes.cart__item_remove}>
-              <div class="button button--outline button--circle">
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
-              </div>
-            </div>
+      </div>
+      <div class={classes.content__item}>
+        <div className={classes.cart__item}>
+          <div className={classes.cart__item_img}>
+            <img src="./assets/img/sets/set_banzai.png" alt="cartItem" />
           </div>
-        </div>
-        <div class={classes.cart__bottom}>
-          <div class={classes.cart__bottom_details}>
-            <span>
-              Всего пицц: <b>3 шт.</b>
-            </span>
-            <span>
-              Сумма заказа: <b>900 ₽</b>
-            </span>
+          <div class={classes.cart__item_info}>
+            <h3>Сырный цыпленок</h3>
+            <p>тонкое тесто, 26 см.</p>
           </div>
-          <div class={classes.cart__bottom_buttons}>
-            <a
-              href="/"
-              class="classes.button classes.button__outline classes.button__add go_back_btn"
+          <div class={classes.cart__item_count}>
+            <div
+              class={`${classes.button} button button--outline button--circle ${classes.cart__item_count_minus}`}
             >
-              <svg
-                width="8"
-                height="14"
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 13L1 6.93015L6.86175 1"
-                  stroke="#D3D3D3"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-
-              <span>Вернуться назад</span>
-            </a>
-            <div class={`${classes.button} ${classes.pay_btn}`}>
-              <span>Оплатить сейчас</span>
+              <CartSvgGenerator id="minus" />
             </div>
+            <b>2</b>
+            <div
+              class={`${classes.button} button button--outline button--circle ${classes.cart__item_count_plus}`}
+            >
+              <CartSvgGenerator id="plus" />
+            </div>
+          </div>
+          <div class={classes.cart__item_price}>
+            <b>770 ₽</b>
+          </div>
+          <div class={classes.cart__item_remove}>
+            <div
+              class={`${classes.button} button button--outline button--circle`}
+            >
+              <CartSvgGenerator id="plus" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class={classes.cart__bottom}>
+        <div class={classes.cart__bottom_details}>
+          <span>
+            Всего пицц: <b>3 шт.</b>
+          </span>
+          <span>
+            Сумма заказа: <b>900 ₽</b>
+          </span>
+        </div>
+        <div class={classes.cart__bottom_buttons}>
+          <a
+            href="/"
+            class={`${classes.button} button button--outline button--add ${classes.go_back_btn}`}
+          >
+            <CartSvgGenerator id="grey-arrow-left" />
+            <span>Вернуться назад</span>
+          </a>
+          <div class={`${classes.button} button ${classes.pay_btn}`}>
+            <span>Оплатить сейчас</span>
           </div>
         </div>
       </div>
