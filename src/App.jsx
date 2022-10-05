085b0b14278310a6561594b0cb1042201a6c2bec
Roman Riezanov <romark6@gmail.com>
2022-10-05 15:42:59 +0300

    Added searching field and added logic for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router";
 import Header from "./components/Header/Header";
 import Cart from "./components/pages/Cart/Cart";
 import Error404 from "./components/pages/Error404/Error404";
 import Home from "./components/pages/Home/Home";
+import SearchContext from "./Context/context";
 import "./scss/globals.scss";
 
 const App = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <div>
-      <div className="wrapper">
+    <div className="wrapper">
+      <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 };
