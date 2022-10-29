d0a649dabad8df9d7f86cac1192310c077d3ac8d
Roman Riezanov <romark6@gmail.com>
2022-10-29 18:07:32 +0300

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <Provider store={store}>
     <BrowserRouter>
